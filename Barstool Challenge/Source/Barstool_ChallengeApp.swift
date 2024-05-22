d93034f5326c65fe82e2fe6bf60a3d04ef2317e9
bnicoloulias <bnicoloulias@gmail.com>
2024-05-22 14:16:59 -0500

    Rename ContentView to StoriesView

--- Barstool Challenge/Source/Barstool_ChallengeApp.swift
+++ Barstool Challenge/Source/Barstool_ChallengeApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Barstool_ChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                StoriesView()
+            }
         }
     }
 }
